9eff7d4f1253ef98daa69dd21ba9b1f8a90e465b
liuyu <liuyu@eswincomputing.com>
2024-06-26 16:30:24 +0800

    fix common,work,sched_api
    Change-Id: I24e80ad6c4a149c73f2620c92e0a035861eecf2a

--- tests/kernel/workq/work/src/main.c
+++ tests/kernel/workq/work/src/main.c
@@ -720,13 +720,16 @@ ZTEST(work_1cpu, test_1cpu_running_cancel)
 	 */
 	k_busy_wait(1000 * (ms_timeout + 1));
 
+	/* If use CONFIG_RISCV_ALWAYS_SWITCH_THROUGH_ECALL, this test will pass.
+	 * If not use CONFIG_RISCV_ALWAYS_SWITCH_THROUGH_ECALL, this test will fail:
+	 * zassert_equal(k_timer_status_get(&ctx->timer), 1) move behind
+	 * zassert_true(k_work_cancel_sync(wp, &work_sync)), it will pass.
+	 */
 	/* busy_wait do not give up CPU, can not sync */
-	// zassert_equal(k_timer_status_get(&ctx->timer), 1);
+	zassert_equal(k_timer_status_get(&ctx->timer), 1);
 
 	/* Wait for cancellation to complete. */
 	zassert_true(k_work_cancel_sync(wp, &work_sync));
-
-	zassert_equal(k_timer_status_get(&ctx->timer), 1);
 
 	/* Verify completion */
 	rc = k_sem_take(&sync_sem, K_NO_WAIT);
