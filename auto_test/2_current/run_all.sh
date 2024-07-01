f2332f51ead388b5a3f7b8a28f829c7ec1ef9abc
liuyu <liuyu@eswincomputing.com>
2024-07-01 14:41:14 +0800

    fix auto test
    Change-Id: I3eccbdd52486b0300cbef0e0613a35a6f700ec6e

--- auto_test/2_current/run_all.sh
+++ auto_test/2_current/run_all.sh
@@ -1,70 +1,41 @@
 # !/bin/bash
 
-# if [ $# -eq 0 ]; then
-#   echo "Usage: $0 filename"
-#   exit 1
-# fi
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 filename"
+  exit 1
+fi
 
-# filename=$1
+filename=$1
 
-# if [ ! -f "$filename" ]; then
-#   echo "Error: File '$filename' not found."
-#   exit 1
-# fi
+if [ ! -f "$filename" ]; then
+  echo "Error: File '$filename' not found."
+  exit 1
+fi
 
-# # 逐行读取文件，并创建目录
-# while IFS= read -r line; do
-#   if [ -n "$line" ]; then
-#     echo "Starting test: $line"
-#     start_time=$SECONDS
-
-    cd /home/user/work/zephyr/src/zephyr/tests/kernel/$1/e330/zephyr
-    riscv64-unknown-elf-gdb -x /home/user/work/zephyr/src/zephyr/auto_test/2_current/run.gdb
-    # sleep 10
-
-#     echo "End test: $line"
-#   fi
-# done < "$filename"
-
-#!/bin/bash
+while IFS= read -r line; do
+  if [ -n "$line" ]; then
+    echo "Starting test: $line"
+    start_time=$SECONDS
+    gnome-terminal --maximize --title "kernel-test" -- bash -c "cd /home/user/work/zephyr/src/zephyr/tests/kernel/$line/e330/zephyr;sleep 1;riscv64-unknown-elf-gdb -x /home/user/work/zephyr/src/zephyr/auto_test/2_current/run.gdb"
 
 
-# if [ $# -eq 0 ]; then
-#   echo "Usage: $0 filename"
-#   exit 1
-# fi
-
-# filename=$1
-
-# if [ ! -f "$filename" ]; then
-#   echo "Error: File '$filename' not found."
-#   exit 1
-# fi
-
-# while IFS= read -r line; do
-#   if [ -n "$line" ]; then
-#     echo "Starting test: $line"
-#     start_time=$SECONDS
-#     gnome-terminal --maximize --title "kernel-test" -- bash -c "cd /home/user/work/zephyr/src/zephyr/tests/kernel/$line/e330/zephyr;sleep 1;riscv64-unknown-elf-gdb -x /home/user/work/zephyr/src/zephyr/auto_test/2_current/run.gdb"
-
-
-#     sleep 10
-#     while true; do
-#         last_line=$(tail -n 1 "/home/user/work/zephyr/src/zephyr/auto_test/2_current/test.log")
-#         fail_line="PROJECT EXECUTION FAILED"
-#         sucess_line="PROJECT EXECUTION SUCCESSFUL"
-#         if [[ *"$last_line"* == *"$fail_line"* || *"$last_line"* == *"$sucess_line"* ]]; then
-#             break
-#         fi
-#         end_time=$SECONDS
-#         run_time=$((end_time - start_time))
-#         if [ $run_time -gt 60 ]; then
-#             echo "timeout test: $line"
-#             break
-#         fi
-#     done
-#     # kill -9 $gdb_pid
-#     xdotool search --name "kernel-test" windowclose
-#     echo "End test: $line"
-#   fi
-# done < "$filename"
+    sleep 10
+    while true; do
+        last_line=$(tail -n 1 "/home/user/work/zephyr/src/zephyr/auto_test/2_current/test.log")
+        fail_line="PROJECT EXECUTION FAILED"
+        sucess_line="PROJECT EXECUTION SUCCESSFUL"
+        if [[ *"$last_line"* == *"$fail_line"* || *"$last_line"* == *"$sucess_line"* ]]; then
+            break
+        fi
+        end_time=$SECONDS
+        run_time=$((end_time - start_time))
+        if [ $run_time -gt 180 ]; then
+            echo "timeout test: $line"
+            break
+        fi
+    done
+    # kill -9 $gdb_pid
+    xdotool search --name "kernel-test" windowclose
+    echo "End test: $line"
+  fi
+done < "$filename"
